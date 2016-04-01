c7a19912a8c86f211982509bd2a78c9b18170121
Dave Gravelle <dgravelle54@gmail.com>
2016-04-01 16:05:20 -0700

    knex in place

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,9 @@
-// Update with your config settings.
-
 module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/galvanize_reads'
   },
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
